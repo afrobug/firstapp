611a35999994aae7f06da8271c91f8c984f0777a
afrobug <afrojahmed6@gmail.com>
2023-01-10 15:34:13 +0530

    school

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {/* <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-        <p>Shaktimaan</p>
-      </header>
+
+      </header> */}
+
+      <header>High school</header>
+      <p>app</p>
+      <footer>contact us</footer>
+
     </div>
   );
 }
-
 export default App;
