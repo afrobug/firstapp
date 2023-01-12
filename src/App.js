bf064d1900055b416a7ad545bb903fda0c61e8ac
afrobug <afrojahmed6@gmail.com>
2023-01-12 23:48:52 +0530

    header

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-
+// import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <Header />
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
 
-      </header> */}
-
+      </header>
       <header>High school</header>
       <p>app</p>
       <footer>contact us</footer>
-
     </div>
   );
 }
